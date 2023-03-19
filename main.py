e3b929b565ad37dc5db23dcf289f2772af288a0d
mrfakename <me@mrfake.name>
2023-03-18 17:44:30 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,41 +1,40 @@
-print("importing...")
 import torch
 from transformers import GPT2Tokenizer, GPT2LMHeadModel
-print("setting up...")
+
 # Set up the tokenizer and model
 tokenizer = GPT2Tokenizer.from_pretrained('gpt2')
 model = GPT2LMHeadModel.from_pretrained('gpt2')
-print("reading data...")
+
 # Load the training data
 with open('traindata.txt', 'r', encoding='utf-8') as f:
     train_data = f.read()
-print("encoding...")
-# Encode the training data
-encodings = tokenizer.encode_plus(train_data, return_tensors='pt')
-print("setting up parameters (setting up adam 1/3)...")
+
+# Split the training data into smaller chunks
+max_length = model.config.n_positions
+train_chunks = [train_data[i:i+max_length] for i in range(0, len(train_data), max_length)]
+
 # Set up the training parameters
 optimizer = torch.optim.Adam(model.parameters(), lr=5e-5)
-print("setting up parameters (setting up cuda/mps 2/3)...")
-device = torch.device('cuda' if torch.cuda.is_available() else 'mps') # Comment out this line for CPU
-print("setting up parameters (piping to cuda/mps 3/3)...")
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 model.to(device)
-print("initializing training...")
-# Fine-tune the model
+
+# Fine-tune the model on each training chunk
 model.train()
-print("starting training...")
-for i in range(100):
-    print("starting epoch " + str(i) + "...")
+for i, chunk in enumerate(train_chunks):
+    print(f"Processing chunk {i+1}/{len(train_chunks)}...")
+    encodings = tokenizer.encode_plus(chunk, return_tensors='pt', max_length=max_length)
     input_ids = encodings['input_ids'].to(device)
-    labels = input_ids.to(device)
+    labels = input_ids.clone().detach()
+    labels[labels == tokenizer.pad_token_id] = -100
+
     outputs = model(input_ids, labels=labels)
     loss = outputs[0]
     loss.backward()
+
     optimizer.step()
     optimizer.zero_grad()
-    print("finished epoch " + str(i) + "...")
+
+    print(f"Processed chunk {i+1}/{len(train_chunks)}. Loss: {loss.item()}")
 
 # Save the fine-tuned model to disk
-print("saving model...")
 model.save_pretrained('finetuned_gpt2')
-print("saved model...")
-
